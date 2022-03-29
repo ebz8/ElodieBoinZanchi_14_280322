553f9ca0d8b13f1665bd84ca1a1db39c1c6762b6
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-03-29 17:54:08 +0200

    style form

--- my-app/src/components/EmployeeForm/EmployeeForm.tsx
+++ my-app/src/components/EmployeeForm/EmployeeForm.tsx
@@ -24,23 +24,46 @@ export default function EmployeeForm() {
     formState: { errors },
   } = useForm<FormValues>()
   const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data)
+  console.log(errors)
 
   return (
     <form className="employee-form" onSubmit={handleSubmit(onSubmit)}>
       <div className="form-group firstName">
-        <label htmlFor="firstName">First Name</label>
-        <input
-          type="text"
-          {...register("firstName", { required: true, maxLength: 80 })}
-        />
+        <div className="fields">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            type="text"
+            {...register("firstName", {
+              required: "Please enter a first name.",
+              minLength: {
+                value: 2,
+                message: "Please enter 2 characters min.",
+              },
+            })}
+          />
+        </div>
+        {errors.firstName && (
+          <div className="form-error">{errors.firstName.message}</div>
+        )}
       </div>
 
       <div className="form-group lastName">
-        <label htmlFor="lastName">Last Name</label>
-        <input
-          type="text"
-          {...register("lastName", { required: true, maxLength: 100 })}
-        />
+        <div className="fields">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            type="text"
+            {...register("lastName", {
+              required: "Please enter a last name.",
+              minLength: {
+                value: 2,
+                message: "Please enter 2 characters min.",
+              },
+            })}
+          />
+        </div>
+        {errors.lastName && (
+          <div className="form-error">{errors.lastName.message}</div>
+        )}
       </div>
 
       <Controller
@@ -48,11 +71,18 @@ export default function EmployeeForm() {
         control={control}
         defaultValue={undefined}
         render={({ field }) => (
-          <DatePicker
-            className="birthDate"
-            onChange={(e) => field.onChange(e)}
-            selected={field.value}
-          />
+          <div className="form-group birthDate">
+            <div className="fields">
+              <label htmlFor="birthDate">Birth Date</label>
+              <DatePicker
+                onChange={(e) => field.onChange(e)}
+                selected={field.value}
+              />
+            </div>
+            {errors.birthDate && (
+              <div className="form-error">{errors.birthDate.message}</div>
+            )}
+          </div>
         )}
       />
 
@@ -61,51 +91,83 @@ export default function EmployeeForm() {
         control={control}
         defaultValue={undefined}
         render={({ field }) => (
-          <DatePicker
-            className="startDate"
-            onChange={(e) => field.onChange(e)}
-            selected={field.value}
-          />
+          <div className="form-group startDate">
+            <div className="fields">
+              <label htmlFor="startDate">Start Date</label>
+              <DatePicker
+                onChange={(e) => field.onChange(e)}
+                selected={field.value}
+              />
+            </div>
+            {errors.startDate && (
+              <div className="form-error">{errors.startDate.message}</div>
+            )}
+          </div>
         )}
       />
 
       {/* Address block */}
       <div className="address">
         <div className="form-group street">
-          <label htmlFor="street">Street</label>
-          <input type="text" {...register("street", {})} />
+          <div className="fields">
+            <label htmlFor="street">Street</label>
+            <input type="text" {...register("street", {})} />
+          </div>
+          {errors.street && (
+            <div className="form-error">{errors.street.message}</div>
+          )}
         </div>
 
         <div className="form-group city">
-          <label htmlFor="city">City</label>
-          <input type="text" {...register("city", {})} />
+          <div className="fields">
+            <label htmlFor="city">City</label>
+            <input type="text" {...register("city", {})} />
+          </div>
+          {errors.city && (
+            <div className="form-error">{errors.city.message}</div>
+          )}
         </div>
 
         <div className="form-group state">
-          <label htmlFor="state">State</label>
-          <select {...register("state", {})}>
-            <option value="Alabama ">Alabama </option>
-            <option value=" Alaska "> Alaska </option>
-            <option value=" American Samoa "> American Samoa </option>
-            <option value=" Arizona"> Arizona</option>
-          </select>
+          <div className="fields">
+            <label htmlFor="state">State</label>
+            <select {...register("state", {})}>
+              <option value="Alabama ">Alabama </option>
+              <option value=" Alaska "> Alaska </option>
+              <option value=" American Samoa "> American Samoa </option>
+              <option value=" Arizona"> Arizona</option>
+            </select>
+          </div>
+          {errors.state && (
+            <div className="form-error">{errors.state.message}</div>
+          )}
         </div>
 
         <div className="form-group zipCode">
-          <label htmlFor="zipCode">Zip Code</label>
-          <input type="number" {...register("zipCode", {})} />
+          <div className="fields">
+            <label htmlFor="zipCode">Zip Code</label>
+            <input type="number" {...register("zipCode", {})} />
+          </div>
+          {errors.zipCode && (
+            <div className="form-error">{errors.zipCode.message}</div>
+          )}
         </div>
       </div>
 
       <div className="form-group department">
-        <label htmlFor="department">Department</label>
-        <select {...register("department", {})}>
-          <option value="Sales ">Sales </option>
-          <option value=" Marketing "> Marketing </option>
-          <option value=" Engineering "> Engineering </option>
-          <option value=" Human Resources "> Human Resources </option>
-          <option value=" Legal"> Legal</option>
-        </select>
+        <div className="fields">
+          <label htmlFor="department">Department</label>
+          <select {...register("department", {})}>
+            <option value="Sales ">Sales </option>
+            <option value=" Marketing "> Marketing </option>
+            <option value=" Engineering "> Engineering </option>
+            <option value=" Human Resources "> Human Resources </option>
+            <option value=" Legal"> Legal</option>
+          </select>
+        </div>
+        {errors.department && (
+          <div className="form-error">{errors.department.message}</div>
+        )}
       </div>
 
       <button className="main-btn" type="submit">
