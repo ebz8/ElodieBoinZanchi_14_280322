fb15803ec052525ce9e0bd1fa5a9d84b4466ccdc
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-03-29 11:34:47 +0200

    add form-groups

--- my-app/src/components/EmployeeForm/EmployeeForm.tsx
+++ my-app/src/components/EmployeeForm/EmployeeForm.tsx
@@ -1,7 +1,8 @@
+import "./EmployeeForm.scss"
+import "react-datepicker/dist/react-datepicker.css"
+
 import { useForm, SubmitHandler, Controller } from "react-hook-form"
 import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
-// SubmitHandlet : use as type
 
 type FormValues = {
   firstName: string
@@ -25,63 +26,91 @@ export default function EmployeeForm() {
   const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data)
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        placeholder="First name"
-        {...register("firstName", { required: true, maxLength: 80 })}
-      />
-      <input
-        type="text"
-        placeholder="Last name"
-        {...register("lastName", { required: true, maxLength: 100 })}
-      />
+    <form className="employee-form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="form-group firstName">
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          {...register("firstName", { required: true, maxLength: 80 })}
+        />
+      </div>
+
+      <div className="form-group lastName">
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          {...register("lastName", { required: true, maxLength: 100 })}
+        />
+      </div>
+
       <Controller
         name="birthDate"
         control={control}
         defaultValue={undefined}
         render={({ field }) => (
           <DatePicker
+            className="birthDate"
             onChange={(e) => field.onChange(e)}
             selected={field.value}
-            placeholderText="Birth Date"
           />
         )}
       />
+
       <Controller
         name="startDate"
         control={control}
         defaultValue={undefined}
         render={({ field }) => (
           <DatePicker
+            className="startDate"
             onChange={(e) => field.onChange(e)}
             selected={field.value}
-            placeholderText="Start Date"
           />
         )}
       />
-      <input type="text" placeholder="Street" {...register("street", {})} />
-      <input type="text" placeholder="City" {...register("city", {})} />
-      <select {...register("state", {})}>
-        <option value="Alabama ">Alabama </option>
-        <option value=" Alaska "> Alaska </option>
-        <option value=" American Samoa "> American Samoa </option>
-        <option value=" Arizona"> Arizona</option>
-      </select>
-      <input
-        type="number"
-        placeholder="Zip Code"
-        {...register("zipCode", {})}
-      />
-      <select {...register("department", {})}>
-        <option value="Sales ">Sales </option>
-        <option value=" Marketing "> Marketing </option>
-        <option value=" Engineering "> Engineering </option>
-        <option value=" Human Resources "> Human Resources </option>
-        <option value=" Legal"> Legal</option>
-      </select>
 
-      <input type="submit" />
+      {/* Address block */}
+      <div className="address">
+        <div className="form-group street">
+          <label htmlFor="street">Street</label>
+          <input type="text" {...register("street", {})} />
+        </div>
+
+        <div className="form-group city">
+          <label htmlFor="city">City</label>
+          <input type="text" {...register("city", {})} />
+        </div>
+
+        <div className="form-group state">
+          <label htmlFor="state">State</label>
+          <select {...register("state", {})}>
+            <option value="Alabama ">Alabama </option>
+            <option value=" Alaska "> Alaska </option>
+            <option value=" American Samoa "> American Samoa </option>
+            <option value=" Arizona"> Arizona</option>
+          </select>
+        </div>
+
+        <div className="form-group zipCode">
+          <label htmlFor="zipCode">Zip Code</label>
+          <input type="number" {...register("zipCode", {})} />
+        </div>
+      </div>
+
+      <div className="form-group department">
+        <label htmlFor="department">Department</label>
+        <select {...register("department", {})}>
+          <option value="Sales ">Sales </option>
+          <option value=" Marketing "> Marketing </option>
+          <option value=" Engineering "> Engineering </option>
+          <option value=" Human Resources "> Human Resources </option>
+          <option value=" Legal"> Legal</option>
+        </select>
+      </div>
+
+      <button className="main-btn" type="submit">
+        Save
+      </button>
     </form>
   )
 }
